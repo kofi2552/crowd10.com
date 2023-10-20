3e3a30d1eeffafa6b7987bd337152191d2628eac
Kofi <kofiloyke2552@gmail.com>
2023-10-20 11:19:06 +0000

    index page added

---
+++ index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="components/navbar/style_nav.css" />
+  <link rel="stylesheet" type="text/css" href="components/footer/style_fot.css" />
+  <link rel="stylesheet" type="text/css" href="style.css" />
+  <title>Crowd10 - Home</title>
+</head>
+<body>
+
+
+  <?php  
+    include("components/navbar/navbar.php"); 
+  ?>
+   
+   <section class="hero-section">
+   <div class="container-fliud">
+   <div class="mobile-show"  style="padding: 10px;">
+          <img src="images/Rectangle 9.png"/>
+        </div>
+    <div class="row" id="home-top">
+      <div class="col-md-6 left hero-content">
+        <!-- Left Div Content -->
+        <div class="mt-5" style="color: white; padding: 20px;" >
+        <div style="width: 100%; height: 100%"><span style="color: white; font-size: 48px; font-family: Work Sans; font-weight: 700; line-height: 58px; word-wrap: break-word">We Are A Crowd Based<br/></span><span style="color: #3CC6F2; font-size: 48px; font-family: Work Sans; font-weight: 700; line-height: 58px; word-wrap: break-word">Real Estate Marketing</span><span style="color: white; font-size: 48px; font-family: Work Sans; font-weight: 700; line-height: 58px; word-wrap: break-word"> Community<br/></span></div>
+          <p class="mt-3">A marketing community established to raise passionate and discipline entrepreneurs from age 18 and above to create wealth in the current era through real estate.</p>
+        <button class="strt-btn">Get Started</button>
+        </div>
+      </div>
+      <div class="col-md-6 right hero-content">
+        <!-- Right Div Content -->
+        <div class="desktop-show" style="padding: 20px;">
+          <img src="images/Rectangle 9.png"/>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="hero-circle">
+        <svg class="arrow" width="60" height="74" viewBox="0 0 60 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M29.8412 72.3481V2.20786M1.64326 41.6617L29.8412 72.3481L58.0391 41.6617" stroke="black" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+      </div>
+    </div>
+  </div>
+   </section>
+
+   <section class="counter-section">
+   <div class="container-fliud">
+    <div class="row">
+      <div class="col-md-12" style=" background: #3CC6F2; border-radius: 10px">
+        <div class="counter-main-content">
+        <div class="col-md-4 counter-1">
+          <h1>15+</h1>
+          <p>This div will appear on the left on larger screens and on top on smaller screens.</p>
+          <div style="width: 80%; margin: 0 auto; height: 100%; border: 1px #011226 solid"></div>
+        </div>
+        <div class="col-md-4 counter-1">
+          <h1>89k</h1>
+          <p>This div will appear on the left on larger screens and on top on smaller screens.</p>
+          <div style="width: 80%; margin: 0 auto; height: 100%; border: 1px #011226 solid"></div>
+        </div>
+        <div class="col-md-4 counter-1">
+          <h1>49.5k</h1>
+          <p>This div will appear on the left on larger screens and on top on smaller screens.</p>
+          <div style="width: 80%; margin: 0 auto; height: 100%; border: 1px #011226 solid"></div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+   </section>
+
+   <section class="approach-section">
+     <div class="section-heading">
+       <h1><span style="color: white; font-size: 48px; font-family: Work Sans; font-weight: 700; line-height: 58px; word-wrap: break-word">Our Approach in<br/></span>
+       <span style="color: #3CC6F2; font-size: 48px; font-family: Work Sans; font-weight: 700; line-height: 58px; word-wrap: break-word">3 Simple Steps</span></h1>
+      </div>
+      <div class="container-fliud" id="appr-head">
+            <div class="row">
+                  <div class="col-md-4">
+                    <div class="box" style="padding: 20px;">
+                    <h1 class="text-center" style="color: white; font-size: 32px; font-weight: 700; line-height: 47px; word-wrap: break-word">Learn</h1>
+                    <p style="text-align: center; color: white; font-size: 16px; font-weight: 400; line-height: 31.50px; word-wrap: break-word">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis maecenas eget aliquam ipsum neque interdum iaculis non.
+                    </p>
+                  </div>
+                  </div>
+
+                  <div class="col-md-4">
+                  <div class="box" style="padding: 20px;">
+                  <h1 class="text-center" style="color: white; font-size: 32px; font-weight: 700; line-height: 47px; word-wrap: break-word">Invest</h1>
+                    <p style="text-align: center; color: white; font-size: 16px; font-family: Work Sans; font-weight: 400; line-height: 31.50px; word-wrap: break-word">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis maecenas eget aliquam ipsum neque interdum iaculis non.</p>
+                  </div>
+                  </div>
+
+                  <div class="col-md-4">
+                  <div class="box" style="padding: 20px;">
+                  <h1 class="text-center" style="color: white; font-size: 32px; font-weight: 700; line-height: 47px; word-wrap: break-word">Xperience</h1>
+                    <p style="text-align: center; color: white; font-size: 16px; font-family: Work Sans; font-weight: 400; line-height: 31.50px; word-wrap: break-word">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis maecenas eget aliquam ipsum neque interdum iaculis non.</p>
+                  </div>
+                  </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="belt-section">
+    </section>
+
+    <section class="whyC10-section">
+     <div class="section-heading">
+       <h1><span style="color: white; font-size: 48px; font-family: Work Sans; font-weight: 700; line-height: 58px; word-wrap: break-word">Why Crowd10 and<br/></span>
+       <span style="color: #3CC6F2; font-size: 48px; font-family: Work Sans; font-weight: 700; line-height: 58px; word-wrap: break-word">Real Estate?</span></h1>
+      </div>
+
+      <div class="container-fliud">
+            <div class="row accordion pt-3">
+                  <div class="col-md-6 left">
+                    <h4 id="element1">01.</h4>
+                    <div class="box">
+                    <h1 id="element1">The Only Real Estate</h1>
+                    <p>Search Engine</p>
+                    </div>
+                    <div class="middle accordion-button"><svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path id="element1" d="M49.538 24.7676C49.538 31.2654 46.9567 37.4971 42.3621 42.0917C37.7674 46.6863 31.5358 49.2676 25.038 49.2676C18.5402 49.2676 12.3085 46.6863 7.71385 42.0917C3.11921 37.4971 0.537964 31.2654 0.537964 24.7676C0.537964 18.2698 3.11921 12.0381 7.71385 7.44346C12.3085 2.84882 18.5402 0.267578 25.038 0.267578C31.5358 0.267578 37.7674 2.84882 42.3621 7.44346C46.9567 12.0381 49.538 18.2698 49.538 24.7676V24.7676ZM26.5692 14.0488C26.5692 13.6427 26.4079 13.2532 26.1207 12.9661C25.8336 12.6789 25.4441 12.5176 25.038 12.5176C24.6319 12.5176 24.2424 12.6789 23.9552 12.9661C23.668 13.2532 23.5067 13.6427 23.5067 14.0488L23.5067 31.7899L16.9346 25.2147C16.6471 24.9272 16.2571 24.7656 15.8505 24.7656C15.4438 24.7656 15.0539 24.9272 14.7663 25.2147C14.4788 25.5022 14.3173 25.8922 14.3173 26.2988C14.3173 26.7055 14.4788 27.0954 14.7663 27.383L23.9538 36.5705C24.0961 36.7131 24.2651 36.8262 24.4511 36.9034C24.6371 36.9806 24.8366 37.0203 25.038 37.0203C25.2394 37.0203 25.4388 36.9806 25.6248 36.9034C25.8109 36.8262 25.9798 36.7131 26.1221 36.5705L35.3096 27.383C35.5971 27.0954 35.7586 26.7055 35.7586 26.2988C35.7586 25.8922 35.5971 25.5022 35.3096 25.2147C35.0221 24.9272 34.6321 24.7656 34.2255 24.7656C33.8188 24.7656 33.4289 24.9272 33.1413 25.2147L26.5692 31.7899L26.5692 14.0488Z" fill="#3CC6F2"/>
+                  </svg>
+                </div>
+                  </div>
+                    
+                  <div class="col-md-6 right">
+                  
+                  <div class="box">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis maecenas eget aliquam ipsum neque interdum iaculis non.</p>
+                  </div>
+                  </div>
+                  <div id="hiddenDiv"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis maecenas eget aliquam ipsum neque interdum iaculis non.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis maecenas eget aliquam ipsum neque interdum iaculis non.</p>
+                  </div>
+            </div>
+            <div class="row accordion pt-3">
+                  <div class="col-md-6 left">
+                    <h4 id="element1">02.</h4>
+                    <div class="box">
+                    <h1 id="element1">The Only Real Estate</h1>
+                    <p>Search Engine</p>
+                    </div>
+                    <div class="middle accordion-button"><svg  width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path id="element1" d="M49.538 24.7676C49.538 31.2654 46.9567 37.4971 42.3621 42.0917C37.7674 46.6863 31.5358 49.2676 25.038 49.2676C18.5402 49.2676 12.3085 46.6863 7.71385 42.0917C3.11921 37.4971 0.537964 31.2654 0.537964 24.7676C0.537964 18.2698 3.11921 12.0381 7.71385 7.44346C12.3085 2.84882 18.5402 0.267578 25.038 0.267578C31.5358 0.267578 37.7674 2.84882 42.3621 7.44346C46.9567 12.0381 49.538 18.2698 49.538 24.7676V24.7676ZM26.5692 14.0488C26.5692 13.6427 26.4079 13.2532 26.1207 12.9661C25.8336 12.6789 25.4441 12.5176 25.038 12.5176C24.6319 12.5176 24.2424 12.6789 23.9552 12.9661C23.668 13.2532 23.5067 13.6427 23.5067 14.0488L23.5067 31.7899L16.9346 25.2147C16.6471 24.9272 16.2571 24.7656 15.8505 24.7656C15.4438 24.7656 15.0539 24.9272 14.7663 25.2147C14.4788 25.5022 14.3173 25.8922 14.3173 26.2988C14.3173 26.7055 14.4788 27.0954 14.7663 27.383L23.9538 36.5705C24.0961 36.7131 24.2651 36.8262 24.4511 36.9034C24.6371 36.9806 24.8366 37.0203 25.038 37.0203C25.2394 37.0203 25.4388 36.9806 25.6248 36.9034C25.8109 36.8262 25.9798 36.7131 26.1221 36.5705L35.3096 27.383C35.5971 27.0954 35.7586 26.7055 35.7586 26.2988C35.7586 25.8922 35.5971 25.5022 35.3096 25.2147C35.0221 24.9272 34.6321 24.7656 34.2255 24.7656C33.8188 24.7656 33.4289 24.9272 33.1413 25.2147L26.5692 31.7899L26.5692 14.0488Z" fill="#3CC6F2"/>
+                  </svg></div>
+                  </div>
+                    
+                  <div class="col-md-6 right">
+                  <div class="box">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis maecenas eget aliquam ipsum neque interdum iaculis non.</p>
+                  </div>
+                  </div>
+                  <div id="hiddenDiv"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis maecenas eget aliquam ipsum neque interdum iaculis non.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis maecenas eget aliquam ipsum neque interdum iaculis non.</p>
+                  </div>
+            </div>
+            <div class="row accordion pt-3">
+                  <div class="col-md-6 left">
+                    <h4 id="element1">03.</h4>
+                    <div class="box">
+                    <h1 id="element1">The Only Real Estate</h1>
+                    <p>Search Engine</p>
+                    </div>
+                    <div class="middle accordion-button"><svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path id="element1" d="M49.538 24.7676C49.538 31.2654 46.9567 37.4971 42.3621 42.0917C37.7674 46.6863 31.5358 49.2676 25.038 49.2676C18.5402 49.2676 12.3085 46.6863 7.71385 42.0917C3.11921 37.4971 0.537964 31.2654 0.537964 24.7676C0.537964 18.2698 3.11921 12.0381 7.71385 7.44346C12.3085 2.84882 18.5402 0.267578 25.038 0.267578C31.5358 0.267578 37.7674 2.84882 42.3621 7.44346C46.9567 12.0381 49.538 18.2698 49.538 24.7676V24.7676ZM26.5692 14.0488C26.5692 13.6427 26.4079 13.2532 26.1207 12.9661C25.8336 12.6789 25.4441 12.5176 25.038 12.5176C24.6319 12.5176 24.2424 12.6789 23.9552 12.9661C23.668 13.2532 23.5067 13.6427 23.5067 14.0488L23.5067 31.7899L16.9346 25.2147C16.6471 24.9272 16.2571 24.7656 15.8505 24.7656C15.4438 24.7656 15.0539 24.9272 14.7663 25.2147C14.4788 25.5022 14.3173 25.8922 14.3173 26.2988C14.3173 26.7055 14.4788 27.0954 14.7663 27.383L23.9538 36.5705C24.0961 36.7131 24.2651 36.8262 24.4511 36.9034C24.6371 36.9806 24.8366 37.0203 25.038 37.0203C25.2394 37.0203 25.4388 36.9806 25.6248 36.9034C25.8109 36.8262 25.9798 36.7131 26.1221 36.5705L35.3096 27.383C35.5971 27.0954 35.7586 26.7055 35.7586 26.2988C35.7586 25.8922 35.5971 25.5022 35.3096 25.2147C35.0221 24.9272 34.6321 24.7656 34.2255 24.7656C33.8188 24.7656 33.4289 24.9272 33.1413 25.2147L26.5692 31.7899L26.5692 14.0488Z" fill="#3CC6F2"/>
+                  </svg>
+                </div>
+                  </div>
+                    
+                  <div class="col-md-6 right">
+                  <div class="box">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis maecenas eget aliquam ipsum neque interdum iaculis non.</p>
+                  </div>
+                  </div>
+                  <div id="hiddenDiv"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis maecenas eget aliquam ipsum neque interdum iaculis non.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis maecenas eget aliquam ipsum neque interdum iaculis non.</p>
+                  </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+  
+    </section>
+
+    <?php  
+    include("components/footer/footer.php"); 
+  ?>
+
+   <script type="text/javascript" src="js/mobile.js"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
